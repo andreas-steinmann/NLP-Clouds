5cfdc1bca04edfd34f24f0c2c8f2157d51bf936a
andreas-steinmann <26360465+andreas-steinmann@users.noreply.github.com>
2024-09-16 07:29:09 +0200

    setup clean text function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,31 @@
 import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+import re
+
+# Datensatz
 file_path = 'data/complaints_processed.csv'
+
+# Pandas - CSV in DataFrame einlesen
 df = pd.read_csv(file_path)
-print(df)
+
+# Zeilen mit fehlenden Beschwerden löschen
+df.dropna(subset=['narrative'], inplace=True)
+
+# Stopwords laden
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+
+# Funktion für „saubere“ Texte
+def clean_text(text):
+    text = text.lower() # Kleinschreibung vom Text
+    text = re.sub(r'[^a-z\s]', '', text)  # Großbuchstaben, Zahlen, Satzzeichen und andere Sonderzeichen werden aus dem Text gelöscht
+    text = ' '.join([word for word in text.split() if word not in stop_words]) # Vorverarbeitung der Daten mittels stopwords
+    return text
+
+# Funktion „saubere“ Texte aufrufen
+df['cleaned_narrative'] = df['narrative'].apply(clean_text)
+
+# Austesten
+print(df.head(20))
+print(stop_words)
