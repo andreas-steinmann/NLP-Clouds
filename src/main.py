b748ed943790f1d3bb7db6fcba18663a81fc2929
andreas-steinmann <26360465+andreas-steinmann@users.noreply.github.com>
2024-09-17 21:09:39 +0200

    wrapper main

--- src/main.py
+++ src/main.py
@@ -3,29 +3,33 @@ import nltk
 from nltk.corpus import stopwords
 import re
 
-# Datensatz
-file_path = 'data/complaints_processed.csv'
+def main():
+    # Datensatz
+    file_path = 'data/complaints_processed.csv'
 
-# Pandas - CSV in DataFrame einlesen
-df = pd.read_csv(file_path)
+    # Pandas - CSV in DataFrame einlesen
+    df = pd.read_csv(file_path)
 
-# Zeilen mit fehlenden Beschwerden löschen
-df.dropna(subset=['narrative'], inplace=True)
+    # Zeilen mit fehlenden Beschwerden löschen
+    df.dropna(subset=['narrative'], inplace=True)
 
-# Stopwords laden
-nltk.download('stopwords')
-stop_words = set(stopwords.words('english'))
+    # Stopwords laden
+    nltk.download('stopwords')
+    stop_words = set(stopwords.words('english'))
 
-# Funktion für „saubere“ Texte
-def clean_text(text):
-    text = text.lower() # Kleinschreibung vom Text
-    text = re.sub(r'[^a-z\s]', '', text)  # Großbuchstaben, Zahlen, Satzzeichen und andere Sonderzeichen werden aus dem Text gelöscht
-    text = ' '.join([word for word in text.split() if word not in stop_words]) # Vorverarbeitung der Daten mittels stopwords
-    return text
+    # Funktion für „saubere“ Texte
+    def clean_text(text):
+        text = text.lower() # Kleinschreibung vom Text
+        text = re.sub(r'[^a-z\s]', '', text)  # Großbuchstaben, Zahlen, Satzzeichen und andere Sonderzeichen werden aus dem Text gelöscht
+        text = ' '.join([word for word in text.split() if word not in stop_words]) # Vorverarbeitung der Daten mittels stopwords
+        return text
 
-# Funktion „saubere“ Texte aufrufen
-df['cleaned_narrative'] = df['narrative'].apply(clean_text)
+    # Funktion „saubere“ Texte aufrufen
+    df['cleaned_narrative'] = df['narrative'].apply(clean_text)
 
-# Austesten
-print(df.head(20))
-print(stop_words)
+    # Austesten
+    print(df.head(20))
+    print(stop_words)
+
+if __name__ == '__main__':
+    main()
